b2be3154096ab0d69350e0eb0f9c140f4a5090c9
Alex <alex.tmcmanus@gmail.com>
2019-08-06 21:48:47 -0400

    added boost and obs gen

--- track.py
+++ track.py
@@ -11,8 +11,20 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import traj
 
-x0 = np.array([0,0,1,1,2,25])
+def formObs_rbe(x, o):
+    px = x[0] - o[0]
+    py = x[1] - o[1]
+    pz = x[2] - o[2]
+    r = np.sqrt(px*px + py*py + pz*pz)
+    b = np.arctan2(py, px)
+    e = np.arctan2(pz, np.sqrt(px*px + py*py))
+    return r,b,e
+    
+
+x0 = np.array([0,0,1,30, 30, 30])
 y = traj.traj_ballistic(10, x0)
+o = np.array([100,0,0])
+#y = traj.traj_boost(25, x0)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -26,12 +38,15 @@ px = x[0]
 py = x[1]
 pz = x[2]
 
+r, b, e = formObs_rbe(x, o)
+
 ax.scatter(px, py, pz, c='r', marker='.')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 
-ax.set_xlim([-5, 5])
-ax.set_ylim([-10, 10])
+#ax.set_xlim([-5, 5])
+#ax.set_ylim([-10, 10])
+ax.scatter(o[0], o[1], o[2], c='b', marker='*')
 
 plt.show()
--- traj.py
+++ traj.py
@@ -24,3 +24,28 @@ def traj_ballistic(tf, x0):
     y = integrate.solve_ivp(traj_dxdt_ballistic, (0, tf), x0, events=[at_ground,apex], dense_output=True)
 
     return y
+
+def traj_dxdt_boost(t, x):
+    gravity = c.gravity()
+    if t < 5:
+        thrust = 75
+    else:
+        thrust = 0
+    v = x[3:]
+    
+    a_grav = gravity* np.array([0,0,1])
+    a_thrust = thrust * (v/np.linalg.norm(v))
+    
+    accel = a_grav + a_thrust
+    
+    dx = np.concatenate((v, accel))
+    
+    return dx
+
+def traj_boost(tf, x0):
+    
+    at_ground.terminal=True
+    y = integrate.solve_ivp(traj_dxdt_boost, (0, tf), x0, events=[at_ground,apex], dense_output=True)
+    
+    return y
+    
